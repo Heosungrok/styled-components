0b707e244458afa2cd9589cd9b50817edfa14f71
hsr1205 <hsr1205@naver.com>
2019-08-30 14:28:44 +0900

    1. 지금까지 해온 방

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import "./app.css";
+
+class App extends Component {
+  render(){
+    return(
+      <Fragment>
+      <button className="button--success">Hello</button>
+      <button className="button--danger">Hello</button>
+      </Fragment>
+
+    )
+  }
+}
+
+
+export default App;
